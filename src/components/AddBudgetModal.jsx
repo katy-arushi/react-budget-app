ff4adfc89c3c25aa88ec632904525f0fb2c8c222
Arushi Katyal <katy.arushi@gmail.com>
2022-03-22 21:36:20 -0400

    fix misspelling

--- src/components/AddBudgetModal.jsx
+++ src/components/AddBudgetModal.jsx
@@ -26,11 +26,11 @@ export default function AddBudgetModal({ show, handleClose }) {
         <Modal.Body>
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
-            <Form.Control red={nameRef} type="text" required/>
+            <Form.Control ref={nameRef} type="text" required />
           </Form.Group>
           <Form.Group className="mb-3" controlId="max">
             <Form.Label>Maximum Spending</Form.Label>
-            <Form.Control ref={maxRef} type="number" required min={0} step={.01} />
+            <Form.Control ref={maxRef} type="number" required min={0} step={0.01} />
           </Form.Group>
           <div className="d-flex justify=content-end">
             <Button variant="primary" type="submit">Add</Button>
